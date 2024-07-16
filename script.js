f3255201ae6d8665c59fbf03f1bce42462144431
daniellemes2017 <daniel_lemes2017@outlook.com>
2024-07-16 15:51:37 -0300

    Atualizando a API

--- script.js
+++ script.js
@@ -1,28 +1,32 @@
 const convertButton = document.querySelector(".convert-button")
 const currencySelect = document.querySelector(".currency-select")
 
-function convertValues(){
+async function convertValues(){
     const inputCurrencyValue = document.querySelector(".input-currency").value
     const currencyValueToConvert = document.querySelector(".currency-value-to-convert")
     const currencyValueConverted = document.querySelector(".currency-value")
 
 
+    const data =  await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL").then(response => response.json())
+
+    const dolar = data.USDBRL.high
+    const euro = data.EURBRL.high
     
-    const dolarToday = 5.66
-    const euroToday = 6.08
+    //const dolarToday = 5.66
+    //const euroToday = 6.08
     const libraToday = 7.18
     const pesoToday = 62.0
 
     if(currencySelect.value == "dolar") {
         currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
             style:"currency", currency:"USD"
-        }).format(inputCurrencyValue / dolarToday)
+        }).format(inputCurrencyValue / dolar)
     }
 
     if(currencySelect.value == "euro") {
         currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
             style:"currency", currency:"EUR"
-        }).format(inputCurrencyValue / euroToday)
+        }).format(inputCurrencyValue / euro)
     }
 
     if(currencySelect.value == "libra") {
